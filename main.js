4dff12a7e02113f0b433b5cdef44d4366166ef52
Mandy Alford <mandyalford@gmail.com>
2019-11-02 17:49:17 -0600

    Add interpolation of name inputs into greeting

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 var playGameBtn = document.querySelector("#play-game");
+var playerGreeting = document.querySelector("#greeting")
+var player1Name = document.querySelector("#player-1")
+var player2Name = document.querySelector("#player-2")
 
 playGameBtn.addEventListener("click",function(){
   document.querySelector(".player-form").classList.add("hidden")
   document.querySelector(".game-instructions").classList.remove("hidden")
+  playerGreeting.innerText= `WELCOME ${player1Name.value} AND ${player2Name.value}!`.toUpperCase()
 })
