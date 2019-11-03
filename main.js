0c835b8d82ec79e30b531a70bbb9cfc2a4640dd3
Mandy Alford <mandyalford@gmail.com>
2019-11-02 20:03:27 -0600

    Created a warning that prompts user to anter a name if player 1 input is empty and does not allow the play game button to be clicked until a name has been entered

--- main.js
+++ main.js
@@ -2,9 +2,24 @@ var playGameBtn = document.querySelector("#play-game");
 var playerGreeting = document.querySelector("#greeting")
 var player1Name = document.querySelector("#player-1")
 var player2Name = document.querySelector("#player-2")
+var warning = document.querySelector(".warning")
 
-playGameBtn.addEventListener("click",function(){
+// playGameBtn.addEventListener("click", showInstructions);
+
+function showInstructions(){
   document.querySelector(".player-form").classList.add("hidden")
   document.querySelector(".game-instructions").classList.remove("hidden")
   playerGreeting.innerText= `WELCOME ${player1Name.value} AND ${player2Name.value}!`.toUpperCase()
-})
+}
+
+function enableGameBtn() {
+  // event.preventDefault();
+  if (player1Name.value === "") {
+    console.log("test works");
+    warning.innerHTML = "You must enter a name";
+  } else {
+    showInstructions();
+  }
+}
+
+playGameBtn.addEventListener("click", enableGameBtn)
