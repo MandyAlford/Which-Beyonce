88a57fa9f816a9caae7f953c628721ad6e5b0582
Mandy Alford <mandyalford@gmail.com>
2019-11-04 20:58:04 -0700

    Create addCards function to populate memory-card sections with images

--- main.js
+++ main.js
@@ -21,9 +21,71 @@ function enableGameBtn() {
     playGameBtn.classList.add("hidden")
     document.querySelector("main").style.background = "#F5F7F6"
     document.querySelector(".card-section").classList.remove("hidden")
+    addCards()
   } else {
     this.haveInstructionsBeenShown= true;
     showInstructions();
+  }
+}
+
+function addCards() {
+  var bCards = [
+    {
+      id: "card-a",
+      src: "assets/bey1.jpg",
+      alt: "Bey1"
+    },
+    {
+      id: "card-b",
+      src: "assets/bey2.jpg",
+      alt: "Bey2"
+    },
+    {
+      id: "card-c",
+      src: "assets/bey3.jpg",
+      alt: "Bey3"
+    },
+    {
+      id: "card-d",
+      src: "assets/bey4.jpg",
+      alt: "Bey4"
+    },
+    {
+      id: "card-e",
+      src: "assets/bey5.jpg",
+      alt: "Bey5"
+    },
+    {
+      id: "card-f",
+      src: "assets/bey1.jpg",
+      alt: "Bey1"
+    },
+    {
+      id: "card-g",
+      src: "assets/bey2.jpg",
+      alt: "Bey2"
+    },
+    {
+      id: "card-h",
+      src: "assets/bey3.jpg",
+      alt: "Bey3"
+    },
+    {
+      id: "card-i",
+      src: "assets/bey4.jpg",
+      alt: "Bey4"
+    },
+    {
+      id: "card-j",
+      src: "assets/bey5.jpg",
+      alt: "Bey5"
+    },
+  ]
+
+  for(var i=0; i < 10; i++) {
+    var memoryCard = document.querySelector("#" + bCards[i].id)
+    memoryCard.innerHTML=`<img class="back-face" src="assets/card-front.png" alt="B"/>
+    <img class="front-face" src="${bCards[i].src}" alt="${bCards[i].alt}"/>`
   }
 }
 
